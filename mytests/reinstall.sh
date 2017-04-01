ff9ca8b995b0f0a5a1db06a8ca98aadcdba0a57f
NickVeld <NickVeld@users.noreply.github.com>
2017-04-01 22:13:48 +0300

    Comment reinstall.sh

--- mytests/reinstall.sh
+++ mytests/reinstall.sh
@@ -1,2 +1,2 @@
-rm -r /usr/local/lib/python3.4/dist-packages/sklearn
-python3 setup.py install
+rm -r /usr/local/lib/python3.4/dist-packages/sklearn # Delete old version, tested on Ubuntu 14.04.5 Desktop
+python3 setup.py install # Install new one
